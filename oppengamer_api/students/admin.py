2fdcb005e36cb54989f3a70c26f2eda54fa16b5f
gambit515 <122382344+gambit515@users.noreply.github.com>
2025-02-23 04:21:00 +0700

    changes

--- oppengamer_api/students/admin.py
+++ oppengamer_api/students/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Group, Student
+from .models import Group, Student, AttendanceRecord
 
 # Регистрация модели Group
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description','chat')  # Поля, которые будут отображаться в списке
+    list_display = ('name', 'description', 'chat')  # Поля для отображения
     search_fields = ('name',)  # Поиск по названию группы
     ordering = ('name',)  # Сортировка по названию группы
 
@@ -15,8 +15,23 @@ class StudentAdmin(admin.ModelAdmin):
     list_filter = ('active', 'group')  # Фильтры справа
     search_fields = ('name', 'surname', 'phone', 'telegram_id')  # Поиск по указанным полям
     ordering = ('surname', 'name')  # Сортировка по фамилии и имени
-    readonly_fields = ('student_id',)  # Поле student_id будет только для чтения
 
     def full_name(self, obj):
         return f"{obj.surname} {obj.name} {obj.patronymic or ''}".strip()
-    full_name.short_description = 'Full Name'  # Название столбца в админкеа
+    full_name.short_description = 'Full Name'  # Название столбца в админке
+
+# Регистрация модели AttendanceRecord
+@admin.register(AttendanceRecord)
+class AttendanceRecordAdmin(admin.ModelAdmin):
+    list_display = ('student', 'group', 'timestamp')  # Поля для отображения
+    list_filter = ('group', 'timestamp')  # Фильтры справа
+    search_fields = ('student__name', 'student__surname', 'group__name')  # Поиск по связанным полям
+    ordering = ('-timestamp',)  # Сортировка по времени создания (от новых к старым)
+
+    def student(self, obj):
+        return f"{obj.student.surname} {obj.student.name}"  # Отображение полного имени студента
+    student.short_description = 'Student'
+
+    def group(self, obj):
+        return obj.group.name if obj.group else None  # Отображение названия группы
+    group.short_description = 'Group'
