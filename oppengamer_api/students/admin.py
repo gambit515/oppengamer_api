b765f2d6293567a3f8e67343674d023fbc81e3c8
gambit515 <122382344+gambit515@users.noreply.github.com>
2025-02-23 13:33:10 +0700

    admin.py changes

--- oppengamer_api/students/admin.py
+++ oppengamer_api/students/admin.py
@@ -1,27 +1,17 @@
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
-
 from .models import Group, Student, AttendanceRecord, ScheduleTask
 
-class StudentAdmin(GuardedModelAdmin):
-    list_display = ('name', 'surname', 'group')
-
-class ScheduleTaskAdmin(GuardedModelAdmin):
-    list_display = ('day', 'time', 'action', 'group')
-
-admin.site.register(Student, StudentAdmin)
-admin.site.register(ScheduleTask, ScheduleTaskAdmin)
-
-# Регистрация модели Group
+# Админ-класс для модели Group
 @admin.register(Group)
-class GroupAdmin(admin.ModelAdmin):
+class GroupAdmin(GuardedModelAdmin):
     list_display = ('name', 'description', 'chat')  # Поля для отображения
     search_fields = ('name',)  # Поиск по названию группы
     ordering = ('name',)  # Сортировка по названию группы
 
-# Регистрация модели Student
+# Админ-класс для модели Student
 @admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
+class StudentAdmin(GuardedModelAdmin):
     list_display = ('full_name', 'phone', 'telegram_id', 'active', 'group')  # Поля для отображения
     list_filter = ('active', 'group')  # Фильтры справа
     search_fields = ('name', 'surname', 'phone', 'telegram_id')  # Поиск по указанным полям
@@ -29,28 +19,36 @@ class StudentAdmin(admin.ModelAdmin):
 
     def full_name(self, obj):
         return f"{obj.surname} {obj.name} {obj.patronymic or ''}".strip()
+
     full_name.short_description = 'Full Name'  # Название столбца в админке
 
-# Регистрация модели AttendanceRecord
+# Админ-класс для модели AttendanceRecord
 @admin.register(AttendanceRecord)
-class AttendanceRecordAdmin(admin.ModelAdmin):
-    list_display = ('student', 'group', 'timestamp')  # Поля для отображения
+class AttendanceRecordAdmin(GuardedModelAdmin):
+    list_display = ('get_student_full_name', 'get_group_name', 'timestamp')  # Поля для отображения
     list_filter = ('group', 'timestamp')  # Фильтры справа
     search_fields = ('student__name', 'student__surname', 'group__name')  # Поиск по связанным полям
     ordering = ('-timestamp',)  # Сортировка по времени создания (от новых к старым)
 
-    def student(self, obj):
-        return f"{obj.student.surname} {obj.student.name}"  # Отображение полного имени студента
-    student.short_description = 'Student'
+    def get_student_full_name(self, obj):
+        return f"{obj.student.surname} {obj.student.name}"
 
-    def group(self, obj):
-        return obj.group.name if obj.group else None  # Отображение названия группы
-    group.short_description = 'Group'
+    get_student_full_name.short_description = 'Student'  # Название столбца в админке
 
+    def get_group_name(self, obj):
+        return obj.group.name if obj.group else None
 
+    get_group_name.short_description = 'Group'  # Название столбца в админке
+
+# Админ-класс для модели ScheduleTask
 @admin.register(ScheduleTask)
-class ScheduleTaskAdmin(admin.ModelAdmin):
-    list_display = ('day', 'time', 'action', 'chat_id', 'group')  # Отображаемые поля в списке
+class ScheduleTaskAdmin(GuardedModelAdmin):
+    list_display = ('day', 'time', 'action', 'get_chat_id', 'group')  # Отображаемые поля в списке
     list_filter = ('day', 'action')  # Фильтры справа
     search_fields = ('group__name',)  # Поиск по имени группы
     ordering = ('day', 'time')  # Сортировка по дню и времени
+
+    def get_chat_id(self, obj):
+        return obj.group.chat if obj.group else None
+
+    get_chat_id.short_description = 'Chat ID'  # Название столбца в админке
